b1175de76ecce10f40422599b8ad9e368c6b7919
cuhp293 <phuchong292003@gmail.com>
2023-12-23 01:48:46 +0700

    update MaxSubSequence done

--- AdHoc/MaxSubSequence.cpp
+++ AdHoc/MaxSubSequence.cpp
@@ -1,35 +1,41 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int a[1000005];
+const int N = 1e5 + 4;
+
 int n;
-int f[1000005];
+vector<int> vt;
 
+void input() {
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        int tmp;
+        cin >> tmp;
+        vt.push_back(tmp);
+    }
+}
 
+void solve(vector<int> vt, int n) {
+    int sum = 0;
+    int maxSum = 0;
+
+    for (int i = 0; i < n; i++) {
+        sum += vt[i];
+        if (sum < 0) {
+            sum = 0;
+        } else if (sum > maxSum) {
+            maxSum = sum;
+        }
+    }
+    cout << maxSum;
+}
 
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0); cout.tie(0);
 
-    cin >> n;
-    for (int i = 0; i < n; i++) {
-        cin >> a[i];
-    }
-
-    int ans = -2e9;
-
-    for (int i = 1; i <= n; i++) {
-        int sum = 0;
-        for (int j = i; j <= n; j++) {
-//          int sum = 0;
-//          for (int k = i; k <= j; k++)
-//              sum += a[k];
-            sum += a[j];
-            if (sum > ans)
-                ans = sum;
-        }
-    }
-    cout << ans;
+    input();
+    solve(vt, n);
 
     return 0;
 }
