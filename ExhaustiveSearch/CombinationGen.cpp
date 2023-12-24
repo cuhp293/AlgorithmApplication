5cdbaa3b324bdae13a6ecdec2088f106d77ef34d
cuhp293 <phuchong292003@gmail.com>
2023-12-24 08:10:18 +0700

    update CombinationGen done

--- ExhaustiveSearch/CombinationGen.cpp
+++ ExhaustiveSearch/CombinationGen.cpp
@@ -1,16 +1,26 @@
+// Tính C_k_n
+
 #include <bits/stdc++.h>
 using namespace std;
 
 int n, k;
-vector<int> curSubset;
+vector<int> curSubset; // Vector lưu trữ tổ hợp hiện tại
 
+
+// In ra tổ hợp hiện tại
 void printSubset() {
     for (int i : curSubset)
         cout << i << " ";
     cout << endl;
 }
 
+// Thêm từng số vào curSubset đến khi đủ k
 void genSubset(int pos) {
+
+    /* curSubset.empty() trả về true nếu rỗng, false nếu không rỗng
+    ? khi curSubset.empty() return true
+    : khi curSubset.back() return false (trả về phần tử cuối cùng của curSubset)
+    */
     int lastNum = (curSubset.empty() ? 0 : curSubset.back());
     for (int i = lastNum+1; i <= n; i++) {
         curSubset.push_back(i);
